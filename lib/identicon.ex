0929959dc7b880a1b9b0bc877461a726df412409
Paul Bodnar <pbodnar@rennie.com>
2017-08-25 21:33:46 -0600

    add hash_input method

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,11 +8,16 @@ defmodule Identicon do
 
   ## Examples
 
-      iex> Identicon.hello
-      :world
+
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
