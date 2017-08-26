1533ac9ec97800c7ecfb60fcda5bf753e9a7170a
Paul Bodnar <pbodnar@rennie.com>
2017-08-25 23:00:46 -0600

    build_rows to deliver an Identicon.Image with and indexed_list grid key

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -31,9 +31,14 @@ defmodule Identicon do
   end
 
   def build_rows(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
